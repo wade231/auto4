136bf88734b07d51b7e901ce5b17c1a349afa62e
wade231 <122469077+wade231@users.noreply.github.com>
2023-02-14 15:54:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hansaka-Anuhas/AutoFilterBot.git /AutoFilterBot
+  git clone https://github.com/wade231/auto4.git /AutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterBot
